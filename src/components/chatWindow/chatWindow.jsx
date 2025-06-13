4c5492f00a0fc1b584e606e6de4c511620d768fb
Amartya Singh <amartyasinghonly@gmail.com>
2025-06-13 16:14:50 +0530

    Update chatWindow.jsx

--- src/components/chatWindow/chatWindow.jsx
+++ src/components/chatWindow/chatWindow.jsx
@@ -88,7 +88,7 @@ const ChatComponent = () => {
     setMessages((prev) => [...prev, botMessage]);
 
     try {
-      const response = await fetch("/api/chat", {
+      const response = await fetch("http://127.0.0.1:8000/api/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
